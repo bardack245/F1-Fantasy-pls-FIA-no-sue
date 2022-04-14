41b1500fe28361d20c9a0913125b235dda2f5cb0
yuukigerma <yuuki.germa@gmail.com>
2022-04-14 12:51:24 +0200

    Jenson Button (2009)

--- Pagine/pilota.php
+++ Pagine/pilota.php
@@ -33,7 +33,11 @@
                                 <img src='../Media/Logo.svg ' alt='logo image '>
                             </a>
                         </div>
-                        <ul class='menu introtxt'></ul>
+                        <ul class='menu introtxt'>
+                            <li>
+                                <a href='market.php '>Market</a>
+                            </li>
+                        </ul>
                         <div class='cta introtxt '>
                             <a href='login.php' class='button' >LOGIN</a>
                         </div>
@@ -59,6 +63,9 @@
                             <li>
                                 <a href='myteam.php '>My Team</a>
                             </li>
+                            <li>
+                                <a href='market.php '>Market</a>
+                            </li>
                         </ul>
                         <div class='cta introtxt'>
                             <a href='account.php' class='button' >
@@ -79,113 +86,38 @@
     
     ?>
 
-
     <div class = 'mt3'></div>
-    <p class="titolo bigtxt">PILOTI:</p>
-
     <?php
+        $numero = $_GET["numeropilota"];
+
         $conn = new mysqli($db_servername,$db_username,$db_password,$db_name);
         if($conn->connect_error){
             die("<p>Connessione al server non riuscita: ".$conn->connect_error."</p>");
         }
 
-        $myquery = "SELECT Numero, NomePilota, CognomePilota, NazioneP, pilota.ValoreFinale, pilota.NomeScuderia, pilota.Foto, Colore
-                    FROM pilota JOIN scuderia ON pilota.NomeScuderia = scuderia.NomeScuderia";
-
+        $myquery = 'SELECT Numero, NomePilota, CognomePilota, NazioneP, pilota.ValoreIniziale, pilota.ValoreFinale, pilota.NomeScuderia, pilota.Foto, Colore
+                    FROM pilota JOIN scuderia ON pilota.NomeScuderia = scuderia.NomeScuderia
+                    WHERE "$numero" = Numero';
+        
         $ris = $conn->query($myquery) or die("<p>Query fallita! ".$conn->error."</p>");
 
-        $numero = array();
-        $nome = array();
-        $cognome = array();
-        $nazione = array();
-        $valore = array();
-        $scuderia = array();
-        $foto = array();
-        $colore = array();
-        $numeropilota = NULL;
+        $nome = $ris["NomePilota"];
+        $cognome = $ris["CognomePilota"];
+        $nazione = $ris["NazioneP"];
 
-        while($row = $ris->fetch_assoc())
-        {
-            $numero[] = $row["Numero"];
-            $nome[] = $row["NomePilota"];
-            $cognome[] = $row["CognomePilota"];
-            $nazione[] = $row["NazioneP"];
-            $valore[] = $row["ValoreFinale"];
-            $scuderia[] = $row["NomeScuderia"];
-            $foto[] = $row["Foto"];
-            $colore[] = $row["Colore"];
-        }
 
-        for ($temp = 0; $temp < count($numero); $temp++)
-        {
-            echo "<a href='pilota.php?numeropilota=$numero[$temp]' class = 'box-pilota' style = 'background-color: $colore[$temp]' input type='submit'>
-                <div class = 'foto-pilota'>
-                    <img src='$foto[$temp]' alt='$nome[$temp] $cognome[$temp]' >
-                </div>
-                <div class = 'info-pilota'>
-                    <p class='bigtxt'>$nome[$temp] $cognome[$temp] $numero[$temp]</p>
-                    <p class='valore bigtxt'>Valore: $valore[$temp]</p>
-                    <p class='normaltxt'>$nazione[$temp]</p>
-                    <p class='normaltxt'>$scuderia[$temp]</p>
-                </div>
-                </a>
-                <br><br><br>";
-        }
-    ?>
 
-    <p class="titolo bigtxt">SCUDERIE:</p>
 
-    <?php
-        $conn = new mysqli($db_servername,$db_username,$db_password,$db_name);
-        if($conn->connect_error){
-            die("<p>Connessione al server non riuscita: ".$conn->connect_error."</p>");
-        }
 
-        $myquery = "SELECT NomeScuderia, TPNome, TPCognome, Nazione, ValoreFinale, Foto, Colore
-        FROM scuderia";
 
-        $ris = $conn->query($myquery) or die("<p>Query fallita! ".$conn->error."</p>");
 
-        $nome = array();
-        $cognome = array();
-        $nazione = array();
-        $valore = array();
-        $scuderia = array();
-        $foto = array();
-        $colore = array();
 
-        while($row = $ris->fetch_assoc())
-        {
-            $nome[] = $row["TPNome"];
-            $cognome[] = $row["TPCognome"];
-            $nazione[] = $row["Nazione"];
-            $valore[] = $row["ValoreFinale"];
-            $scuderia[] = $row["NomeScuderia"];
-            $foto[] = $row["Foto"];
-            $colore[] = $row["Colore"];
-        }
 
-        for ($temp = 0; $temp < count($scuderia); $temp++)
-        {
-            echo "<a href='scuderia.php' class = 'box-scuderia' style = 'border: 5px solid $colore[$temp]' input type='submit'>
-                <div class = 'foto-pilota'>
-                    <img src='$foto[$temp]' alt='$scuderia[$temp]' >
-                </div>
-                <div class = 'info-scuderia'>
-                    <p class='bigtxt'>$scuderia[$temp] </p>
-                    <p class='valore bigtxt'>Valore: $valore[$temp]</p>
-                    <p class='normaltxt'>$nazione[$temp]</p>
-                    <p class='normaltxt'>$nome[$temp] $cognome[$temp]</p>
-                </div>
-                
-                </a>
-                <br><br><br>";
-        }
 
-    ?>
 
 
 
+    ?>
 
 
 
